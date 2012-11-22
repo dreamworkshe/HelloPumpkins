0c9f34afd6e8b9d805faa1795bc7dc2740bdb1d8
Yao <dreamworkshe@gmail.com>
2012-11-22 15:44:27 -0800

    Test watchposition 2

--- app/assets/javascripts/enter.js
+++ app/assets/javascripts/enter.js
@@ -10,9 +10,10 @@ if (navigator.geolocation) {
     alert('We could not get your location');
   },{
     // It'll stop looking after an hour. Enabling high accuracy tells it to use GPS if it's available  
-    timeout: 1500,
+    timeout: 5000,
     maximumAge: 600000,
-    enableHighAccuracy: true
+    enableHighAccuracy: true,
+    frequency: 5000
   });
 } else {
 	error('not supported');
@@ -39,6 +40,56 @@ channel.bind('pusher:member_removed', function(member) {
 channel.bind('client-123', function(member) {
   console.log('yes');
 });
+
+
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::                                                                         :::
+//:::  This routine calculates the distance between two points (given the     :::
+//:::  latitude/longitude of those points). It is being used to calculate     :::
+//:::  the distance between two locations using GeoDataSource (TM) prodducts  :::
+//:::                                                                         :::
+//:::  Definitions:                                                           :::
+//:::    South latitudes are negative, east longitudes are positive           :::
+//:::                                                                         :::
+//:::  Passed to function:                                                    :::
+//:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
+//:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
+//:::    unit = the unit you desire for results                               :::
+//:::           where: 'M' is statute miles                                   :::
+//:::                  'K' is kilometers (default)                            :::
+//:::                  'N' is nautical miles                                  :::
+//:::                                                                         :::
+//:::  Worldwide cities and other features databases with latitude longitude  :::
+//:::  are available at http://www.geodatasource.com                          :::
+//:::                                                                         :::
+//:::  For enquiries, please contact sales@geodatasource.com                  :::
+//:::                                                                         :::
+//:::  Official Web site: http://www.geodatasource.com                        :::
+//:::                                                                         :::
+//:::               GeoDataSource.com (C) All Rights Reserved 2012            :::
+//:::                                                                         :::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function distance(lat1, lon1, lat2, lon2) {
+  var radlat1 = Math.PI * lat1/180;
+  var radlat2 = Math.PI * lat2/180;
+  var radlon1 = Math.PI * lon1/180;
+  var radlon2 = Math.PI * lon2/180;
+  var theta = lon1-lon2;
+  var radtheta = Math.PI * theta/180;
+  var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  dist = Math.acos(dist);
+  dist = dist * 180/Math.PI;
+  dist = dist * 60 * 1.1515;
+
+  unit = "K";
+  if (unit=="K") { dist = dist * 1.609344; }
+  if (unit=="N") { dist = dist * 0.8684; }
+  return dist;
+}                                                                           
+
+
 
 function error(msg) {
 	alert("error!");
